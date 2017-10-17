0234369fab389f74ac08eaf95fbade42b0772eae
schroffl <schroffl@users.noreply.github.com>
2017-10-17 14:05:10 +0200

    Keep track of connected players

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,20 @@
 
 module Main where
 
-import Control.Concurrent (MVar, newMVar)
+import Control.Concurrent (MVar, modifyMVar_, newMVar)
 import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Network.WebSockets as WS
 import qualified Player as Player
 import System.Environment (getArgs)
 
+type PlayerList = [(WS.Connection, Player.Player)]
+
 main :: IO ()
 main = do
   port <- parsePort <$> getArgs
-  WS.runServer "127.0.0.1" port app
+  playerListMvar <- newMVar [] :: IO (MVar PlayerList)
+  WS.runServer "0.0.0.0" port $ app playerListMvar
 
 parsePort :: [String] -> Int
 parsePort args =
@@ -19,7 +23,12 @@ parsePort args =
     [] -> 9160
     (h:_) -> read h
 
-app :: WS.ServerApp
-app pending = do
+app :: MVar PlayerList -> WS.ServerApp
+app playerListMvar pending = do
   conn <- WS.acceptRequest pending
-  Player.handshake conn
+  Player.handshake conn $ addPlayer playerListMvar conn
+
+addPlayer :: MVar PlayerList -> WS.Connection -> Player.Player -> IO ()
+addPlayer playerListMvar conn player = do
+  print $ Player.plUsername player `T.append` " connected!"
+  modifyMVar_ playerListMvar $ return . (:) (conn, player)
