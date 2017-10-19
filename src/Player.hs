23d96125fe39bf18cd271851678f3e101979d992
schroffl <schroffl@users.noreply.github.com>
2017-10-19 18:40:18 +0200

    Simplified code to first get the basics working and later implement the player handling logic

--- src/Player.hs
+++ src/Player.hs
@@ -1,65 +1,39 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Player
-  ( handshake
-  , Player(plUsername)
-  ) where
+module Player where
 
+import Control.Concurrent
+import Control.Monad (forever)
 import Data.Text (Text)
-import qualified Data.Text as T
 import Messages
 import qualified Network.WebSockets as WS
 
+type Players = [MVar Player]
+
+makeListMVar :: IO (MVar Players)
+makeListMVar = newMVar []
+
+addPlayer :: MVar Players -> MVar Player -> IO ()
+addPlayer playerListVar playerVar =
+  modifyMVar_ playerListVar $ return . (:) playerVar
+
 data Player = Player
   { plUsername :: Text
-  , plKeys :: Keys
   } deriving (Show)
 
-data Keys = Keys
-  { keyForward :: Bool
-  , keyBack :: Bool
-  , keyLeft :: Bool
-  , keyRight :: Bool
-  } deriving (Show)
+makePlayerMVar :: Text -> IO (MVar Player)
+makePlayerMVar username = newMVar $ Player {plUsername = username}
 
-initPlayer :: Text -> Player
-initPlayer username =
-  Player {plUsername = username, plKeys = Keys False False False False}
+handlePlayer :: WS.Connection -> MVar Player -> IO ()
+handlePlayer conn playerVar =
+  forever $ do
+    maybeMsg <- parsePlayerMessage <$> WS.receiveData conn
+    return ()
 
-send :: WS.Connection -> ServerMessage -> IO ()
-send conn = WS.sendTextData conn . encodeServerMessage
+deriveMessages :: Player -> Player -> [ServerMessage]
+deriveMessages oldPlayer newPlayer =
+  []
 
-handshake :: WS.Connection -> (Player -> IO ()) -> IO ()
-handshake conn addPlayer = do
-  maybeMessage <- parsePlayerMessage <$> WS.receiveData conn
-  case maybeMessage of
-    Just (ConnectionRequest username) -> do
-      send conn (Connected username)
-      addPlayer player
-      handlePlayer conn player
-      where player = initPlayer username
-    _ -> handshake conn addPlayer
-
-handlePlayer :: WS.Connection -> Player -> IO ()
-handlePlayer conn player = do
-  maybeMessage <- parsePlayerMessage <$> WS.receiveData conn
-  print maybeMessage
-  case maybeMessage of
-    Nothing -> handlePlayer conn player
-    Just msg -> handlePlayer conn $ update msg player
-
-update :: PlayerMessage -> Player -> Player
-update msg player =
-  case msg of
-    KeyChange keyCode newState -> player {plKeys = newKeys}
-      where newKeys = updateKeys keyCode newState $ plKeys player
-    _ -> player
-
-updateKeys :: Int -> Bool -> Keys -> Keys
-updateKeys keyCode newState keys =
-  case keyCode of
-    87 -> keys {keyForward = newState}
-    83 -> keys {keyBack = newState}
-    65 -> keys {keyLeft = newState}
-    68 -> keys {keyRight = newState}
-    _ -> keys
+-- applyInput :: Player -> Player
+-- applyVelocity :: Player -> Player
+-- deriveMessages :: Player -> Player -> [ServerMessage]
