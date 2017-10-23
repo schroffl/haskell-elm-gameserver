9494262a85103af7501f3dfe8b402edfade4c072
schroffl <schroffl@users.noreply.github.com>
2017-10-23 14:54:52 +0200

    Trying a different approach of using a message queue to apply ANY changes to the game state

--- app/Main.hs
+++ app/Main.hs
@@ -2,25 +2,10 @@
 
 module Main where
 
-import Control.Concurrent (MVar, forkIO)
-import Messages
 import qualified Network.WebSockets as WS
-import qualified Player as Pl
-import qualified Subscribers as Sub
 
 main :: IO ()
-main = do
-  subVar <- Sub.makeMVar
-  playerListVar <- Pl.makeListMVar
-  WS.runServer "0.0.0.0" 9160 $ app subVar playerListVar
+main = WS.runServer "0.0.0.0" 9160 app
 
-app :: MVar Sub.Subscribers -> MVar Pl.Players -> WS.ServerApp
-app subVar playerListVar pending = do
-  conn <- WS.acceptRequest pending
-  maybeMsg <- parsePlayerMessage <$> WS.receiveData conn
-  case maybeMsg of
-    Just (ConnectionRequest uname) -> do
-      playerVar <- Pl.makePlayerMVar uname
-      Sub.addSubscriber subVar conn
-      Pl.addPlayer playerListVar playerVar
-    _ -> return ()
+app :: WS.ServerApp
+app pending = WS.rejectRequest pending "Nope!"
