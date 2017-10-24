3a681ef227d476f3acd680f560a7458a896e0ab4
schroffl <schroffl@users.noreply.github.com>
2017-10-24 16:26:36 +0200

    Fork a thread for the game logic to run in; Then accept connections and hand those off to Player.handlePlayer if the handshake is successful

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,27 @@
 
 module Main where
 
+import Control.Concurrent (forkIO)
+import Control.Concurrent.Chan.Unagi
+import qualified Game
+import MessageParser
 import qualified Network.WebSockets as WS
+import qualified Player
+import Types
+       (ClientMessage(..), GameMessage(..), GameMessageInput)
 
 main :: IO ()
-main = WS.runServer "0.0.0.0" 9160 app
+main = do
+  chan@(input, output) <- newChan
+  forkIO $ Game.start chan
+  WS.runServer "0.0.0.0" 9160 $ app input
 
-app :: WS.ServerApp
-app pending = WS.rejectRequest pending "Nope!"
+app :: GameMessageInput -> WS.ServerApp
+app input pending = do
+  conn <- WS.acceptRequest pending
+  maybeConnReq <- parseClientMessage <$> WS.receiveData conn
+  case maybeConnReq of
+    Just (ConnectionRequest username) -> do
+      writeChan input $ CreatePlayer username
+      Player.handlePlayer conn input username
+    _ -> return ()
